ed349792ab2e01bc861ff295e49fdc997f5f37db
tamatar-23 <gouravkrishna23@gmail.com>
2026-04-01 22:38:25 +0530

    first commit

---
+++ 1928lc.py
@@ -0,0 +1,8 @@
+#1929. Concatenation of Array
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        ans = []
+        for i in range(2):
+            for num in nums:
+                ans.append(num)
+        return ans
